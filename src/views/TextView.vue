b65de66a217146410a0b6fd7303a3c2674ad0688
Ritwik_Raj <111105768+ritwik-raj1@users.noreply.github.com>
2024-04-27 22:44:45 +0530

    minor fix

--- src/views/TextView.vue
+++ src/views/TextView.vue
@@ -61,7 +61,7 @@ function sendQuestion() {
       </div>
       <div
         v-if="textChatStore.gptResponse.length > 0"
-        class="block w-full rounded-md border-0 bg-green-900 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:p-4 sm:text-sm sm:leading-6 text-sm my-4"
+        class="audioblock block w-full rounded-md border-0 bg-green-900 text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:p-4 sm:text-sm sm:leading-6 text-sm my-4"
         style="max-width: 98%;"
         >
         {{ textChatStore.gptResponse }}
